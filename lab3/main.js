a1a9e863d5437a0ebce10946772f5b59d5c134dd
Megan Kuo <61397197+megankuo@users.noreply.github.com>
2020-09-30 01:14:17 -0700

    Update main.js

--- lab3/main.js
+++ lab3/main.js
@@ -1,13 +1,30 @@
-const readlineSync = require('readline-sync');
+const { question, questionInt } = require('readline-sync');
 const makeCalendar = require('./lab-three').makeCalendar;
 const getDayOfTheWeek = require('./lab-three').getDayOfTheWeek;
 
+// Asks for user to input a date to return the corresponding weekday
 function getDayOfTheWeekForUserDate() {
-    const userInputYear = parseInt(readlineSync.question('Please enter the year (yyyy):\n'));
-    const userInputMonth = readlineSync.question('Please enter the month (mmm):\n');
-    const userInputDay = parseInt(readlineSync.question('Please enter the day (d/dd):\n'));
-    console.log(getDayOfTheWeek(userInputYear, userInputMonth, userInputDay));
-    return getDayOfTheWeek(userInputYear, userInputMonth, userInputDay);
+    let userInputDay;
+    let userInputMonth;
+    let userInputYear;
+    do {
+        do {
+            userInputYear = questionInt('Please enter the year (yyyy):\n');
+        } while ( typeof(userInputYear) != 'number' );
+        do {
+            userInputMonth = question('Please enter the month (mmm):\n');
+            if ( userInputMonth.length != 3 ) {
+                console.log('Input valid 3 letter month abbreviation, please');
+            }
+        } while ( userInputMonth.length != 3 );
+        do {
+            userInputDay = questionInt('Please enter the day (d/dd):\n');
+            if ( userInputDay > 31 ) {
+                console.log('Input valid date, please');
+            }
+        } while ( typeof(userInputDay) != 'number' || userInputDay > 31 );
+    } while ( typeof(userInputYear) != 'number' || userInputMonth.length != 3 || typeof(userInputDay) != 'number' );
+    console.log( getDayOfTheWeek( userInputYear, userInputMonth, userInputDay ) );
 }
 
 makeCalendar();
